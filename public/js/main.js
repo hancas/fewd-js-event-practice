62ac8e8bd7372d4539ee8fe639eb3574f7935ad9
Han Castanedo <hancastanedo@gmail.com>
2017-11-15 09:41:54 -0500

    add button events on single listener

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,17 @@
 (function () {
+  const row = document.querySelector('.row');
+  const btn = document.getElementsByTagName('button');
 
-  // your js goes here
+  row.addEventListener('click', handleEvent, false);
 
+  function handleEvent (e) {
+    if (e.target === btn[0]) {
+      btn[0].classList.toggle('button-danger');
+    } else if (e.target === btn[1]) {
+      btn[1].innerText = 'you clicked me';
+    } else {
+      btn[1].innerText = 'button two';
+    }
+    e.stopPropagation();
+  }
 })();
